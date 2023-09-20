1f919b67cfbd08eb3264858358e3e873724e217d
pro100ergeyko <124717620+pro100ergeyko@users.noreply.github.com>
2023-09-20 20:38:44 +0300

    fixed hw

--- src/components/SearchMovies/SearchMovies.jsx
+++ src/components/SearchMovies/SearchMovies.jsx
@@ -1,59 +1,11 @@
-// import { useEffect, useRef, useState } from 'react';
-// import { useSearchParams } from 'react-router-dom';
-// import { toast } from 'react-toastify';
-// import { FcSearch } from 'react-icons/fc';
 import { MoviesForm, MoviesInput } from './SearchMovies.styled';
 
 export const SearchMovies = ({ value, onChange }) => {
   const handleSubmit = e => {
     e.preventDefault();
+    // console.log(value);
+    onChange(e.target.value);
   };
-  // const [nextSerchQuery, setNextSerchQuery] = useState('');
-  // const [prevSerchQuery, setPrevSerchQuery] = useState('');
-  // const [serchParams, setSerchParams] = useSearchParams();
-  // const findMovies = serchParams.get('findMovies') ?? '';
-
-  // const isFirstSearch = useRef(true);
-
-  // const handleChange = evt => {
-  //   const value = evt.currentTarget.value;
-  //   if (value === '') {
-  //     setNextSerchQuery('');
-  //     setSerchParams({});
-  //     return;
-  //   }
-
-  //   setSerchParams({ findMovies: value });
-  //   setNextSerchQuery(value);
-  // };
-
-  // const handleSubmit = evt => {
-  //   evt.preventDefault();
-
-  //   if (!nextSerchQuery) {
-  //     toast.info("👈 Please don't be stupid, enter the search value.");
-  //     return;
-  //   }
-
-  //   if (nextSerchQuery === prevSerchQuery) {
-  //     toast.info(
-  //       `${nextSerchQuery} - be creative 😊, enter a new search value 👍`
-  //     );
-  //     return;
-  //   }
-
-  //   setPrevSerchQuery(nextSerchQuery);
-  //   props.onSubmit(nextSerchQuery);
-  //   setNextSerchQuery('');
-  // };
-
-  // useEffect(() => {
-  //   if (findMovies && isFirstSearch.current) {
-  //     setPrevSerchQuery(findMovies);
-  //     props.onSubmit(findMovies);
-  //   }
-  //   isFirstSearch.current = false;
-  // }, [setPrevSerchQuery, props, findMovies, isFirstSearch]);
 
   return (
     <>
